688d0cd552f7eabe719cfda165eb964ca5c841cf
AlanDAmario <vega93@live.it>
2024-10-07 12:03:54 +0200

    add fix address

--- resources/views/auth/apartments/show.blade.php
+++ resources/views/auth/apartments/show.blade.php
@@ -37,8 +37,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                             </svg>
-                            {{ $apartment->address }}, {{ $apartment->house_number }}, {{ $apartment->postal_code }},
-                            {{ $apartment->country }}
+                            {{ $apartment->address }} 
                         </p>
                     </div>
 
